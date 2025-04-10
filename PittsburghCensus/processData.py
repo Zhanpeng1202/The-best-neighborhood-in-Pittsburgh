b6d9ac8598c5e05a640ee7dc55f285a85770faa4
罗展鹏 <zhl357@pitt.edu>
2025-04-10 14:25:08 -0400

    visualize all in the geopandas

--- PittsburghCensus/processData.py
+++ PittsburghCensus/processData.py
@@ -12,14 +12,14 @@ neighborhood_counts = citations_df["NEIGHBORHOOD"].value_counts().to_dict()
 print(neighborhood_counts)
 
 # make a new column in the gdf called "citation_count" and set it to the neighborhood_counts dictionary
-gdf["citation_count"] = gdf["hood"].map(neighborhood_counts)
+# gdf["citation_count"] = gdf["hood"].map(neighborhood_counts)
 
-# plot the gdf
-gdf.plot(column="citation_count",
-         cmap="Blues",
-         norm=LogNorm(vmin=gdf['citation_count'].min(), vmax=gdf['citation_count'].max()),
-         legend=True)
-plt.show()
+# # plot the gdf
+# gdf.plot(column="citation_count",
+#          cmap="Blues",
+#          norm=LogNorm(vmin=gdf['citation_count'].min(), vmax=gdf['citation_count'].max()),
+#          legend=True)
+# plt.show()
 
 
 
